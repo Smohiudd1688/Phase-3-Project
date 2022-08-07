4173a619a53998c49bb813438648e3e4ac37103a
Sana Mohiuddin <smohiuddin1688@gmail.com>
2022-08-07 13:13:53 -0700

    add some more elements to the form

--- client/my-app-frontend/src/components/ReviewForm.js
+++ client/my-app-frontend/src/components/ReviewForm.js
@@ -1,8 +1,13 @@
 import React, {useState} from "react";
 
 function ReviewForm() {
+    const [rating, setRating] = useState("");
     const [review, setReview] = useState("");
     const [name, setName] = useState("");
+
+    function handleRatingChange(event) {
+        setRating(event.target.value);
+    }
 
     function handleReviewChange(event) {
         setReview(event.target.value);
@@ -12,13 +17,19 @@ function ReviewForm() {
         setName(event.target.value);
     }
 
+    function handleReviewSubmit(event) {
+        event.preventDefault();
+    }
+
     return (
-        <form>
+        <form onSubmit={handleReviewSubmit}>
             <h3>Add a Review</h3>
-            <p className="formQ">What do you think about this show</p>
+            <label htmlFor="rating">How would you rate this show(1-5): </label>
+            <input onChange={handleRatingChange} type="text" id="rating" name="rating" value={rating} /><br></br><br></br>
+            <p className="formQ">What do you think about this show: </p>
             <textarea onChange={handleReviewChange} rows="5" cols="35" id="about" value={review}></textarea><br></br><br></br>
-            <label htmlFor="image">Your name: </label>
-            <input onChange={handleNameChange} type="text" id="image" name="image" value={name} /><br></br><br></br>
+            <label htmlFor="name">Your name: </label>
+            <input onChange={handleNameChange} type="text" id="name" name="name" value={name} /><br></br><br></br>
             <input type="submit" />
         </form>
     )
